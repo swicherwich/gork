4d81fb3a77ed88e6124dd11c97b2046bed84ed0c
Oleh Yashnyi <oleh.yashnyi@gmail.com>
2023-10-15 21:14:46 +0300

    added grouping operation

--- op/op_test.go
+++ op/op_test.go
@@ -30,6 +30,36 @@ func TestMap(t *testing.T) {
 	}
 }
 
+func TestGroup(t *testing.T) {
+	dataset := &ds.Dataset{
+		Headers: []string{"col1", "col2"},
+		Types: map[string]reflect.Kind{
+			"col1": reflect.String, "col2": reflect.Int,
+		},
+		Data: []map[string]any{
+			{"col1": "rec1", "col2": 11},
+			{"col1": "rec2", "col2": 22},
+			{"col1": "rec2", "col2": 33},
+		},
+	}
+	Group[int](dataset, []string{"col1"}, "col2", func(acc *int, a int) { *acc += a })
+
+	want := &ds.Dataset{
+		Headers: []string{"col1", "col2"},
+		Types: map[string]reflect.Kind{
+			"col1": reflect.String, "col2": reflect.Int,
+		},
+		Data: []map[string]any{
+			{"col1": "rec1", "col2": 11},
+			{"col1": "rec2", "col2": 55},
+		},
+	}
+
+	if !reflect.DeepEqual(want, dataset) {
+		t.Fatalf(`Group[int](dataset, []string{"col1"}, "col2", func(acc *int, a int) { *acc += a }) = %v, want %v`, dataset, want)
+	}
+}
+
 func getDataset() ds.Dataset {
 	return ds.Dataset{
 		Headers: []string{"col1", "col2"},
