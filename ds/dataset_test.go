1e1b8f211fb832e54998c067761f1f2107bddc37
Oleh Yashnyi <oleh.yashnyi@gmail.com>
2023-10-15 22:41:14 +0300

    Implemented pretty String function

--- ds/dataset_test.go
+++ ds/dataset_test.go
@@ -8,10 +8,11 @@ import (
 func TestDataset_String(t *testing.T) {
 	dataset := getDataset()
 	res := dataset.String()
-	want := `| <'col1'>[index=0][type=string] | <'col2'>[index=1][type=int] |
-| rec1| 11| 
-| rec2| 22| 
-| rec3| 33| 
+	want := `col1              |col2
+---------------   |---------------
+rec1              |11
+rec2              |22
+rec3              |33
 `
 	if want != res {
 		t.Fatalf("dataset.String() = \n%s, want = \n%s", res, want)
