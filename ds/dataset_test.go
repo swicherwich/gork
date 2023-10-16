5b5e668d2c6fcdae0b9fcbbf6d9c69bf053b142b
Oleh Yashnyi <oleh.yashnyi@gmail.com>
2023-10-16 21:31:57 +0300

    Added saving dataset to file

--- ds/dataset_test.go
+++ ds/dataset_test.go
@@ -1,6 +1,8 @@
 package ds
 
 import (
+	"encoding/csv"
+	"os"
 	"reflect"
 	"testing"
 )
@@ -39,6 +41,69 @@ func TestParseValue(t *testing.T) {
 	val, kind = parseValue("true")
 	if val != true || kind != reflect.Bool {
 		t.Fatalf("parseValue(1) = [val=%s, kind=%s], want = [val=%v, kind=%s]", val, kind, true, reflect.Bool)
+	}
+}
+
+func createTempFile(t *testing.T) *os.File {
+	file, err := os.CreateTemp("", "test.csv")
+	if err != nil {
+		t.Fatal("Error creating temporary file:", err)
+	}
+	return file
+}
+
+func removeTempFile(t *testing.T, file *os.File) {
+	if err := os.Remove(file.Name()); err != nil {
+		t.Fatal("Error removing temporary file:", err)
+	}
+}
+
+func TestSaveCsv(t *testing.T) {
+	ds := Dataset{
+		Headers: []string{"Name", "Age"},
+		Data: []map[string]any{
+			{"Name": "Alice", "Age": 30},
+			{"Name": "Bob", "Age": 25},
+		},
+	}
+
+	file := createTempFile(t)
+	defer removeTempFile(t, file)
+
+	err := ds.SaveCsv(file.Name())
+	if err != nil {
+		t.Fatal("SaveCsv returned an error:", err)
+	}
+
+	file.Close()
+	csvFile, err := os.Open(file.Name())
+	if err != nil {
+		t.Fatal("Error opening the saved CSV file:", err)
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	records, err := reader.ReadAll()
+	if err != nil {
+		t.Fatal("Error reading the saved CSV file:", err)
+	}
+
+	expected := [][]string{
+		{"Name", "Age"},
+		{"Alice", "30"},
+		{"Bob", "25"},
+	}
+
+	if len(records) != len(expected) {
+		t.Fatalf("Expected %d records, but got %d", len(expected), len(records))
+	}
+
+	for i, record := range records {
+		for j, value := range record {
+			if value != expected[i][j] {
+				t.Errorf("Expected value %s at [%d][%d], but got %s", expected[i][j], i, j, value)
+			}
+		}
 	}
 }
 
